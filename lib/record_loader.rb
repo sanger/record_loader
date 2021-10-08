9eae350ddf6e52ca90cf45484acd2a739273ed8a
James Glover <james.glover@sanger.ac.uk>
2021-10-08 14:29:53 +0100

    Fix typos in documentation

--- lib/record_loader.rb
+++ lib/record_loader.rb
@@ -14,12 +14,14 @@ module RecordLoader
   # Raised when it appears RecordLoader is improperly configured
   ConfigurationError = Class.new(StandardError)
 
+  #
   # Helper method to automatically generate config yaml for existing data.
   # This method does not handle setting up associations as the best way of
-  # serializing that information is left up to the individual RecorLoader
+  # serializing that information is left up to the individual RecordLoader
+  #
   # @param [ActiveRecord::Base] model_class A class to serialize
   # @param [String] key The key to index by (eg. 'name')
-  # @retuirn [String] Yaml listing the existing records
+  # @return [String] Yaml listing the existing records
   def self.export_attributes(model_class, key)
     model_class.all.each_with_object({}) do |record, store|
       store[record[key]] = record.attributes.except('id', 'updated_at', 'created_at', key)
