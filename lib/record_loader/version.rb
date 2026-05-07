3aa3f63863477a39b2293af662809cf73919c10b
Stephen Hulme <sh51@sanger.ac.uk>
2026-05-07 14:41:05 +0100

    style: add coverage exclusion for version

--- lib/record_loader/version.rb
+++ lib/record_loader/version.rb
@@ -1,6 +1,10 @@
 # frozen_string_literal: true
 
 module RecordLoader
+  # :nocov: the version number is loaded before coverage recording starts
+
   # @return [String] The current sem-ver version number
   VERSION = '0.3.0'
+
+  # :nocov:
 end
