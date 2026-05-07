1432a7ccbbbb74e923615136ba8084690557c33c
Stephen Hulme <sh51@sanger.ac.uk>
2026-05-07 17:07:37 +0100

    Revert "style: add coverage exclusion for version"
    This reverts commit 3aa3f63863477a39b2293af662809cf73919c10b.

--- lib/record_loader/version.rb
+++ lib/record_loader/version.rb
@@ -1,10 +1,6 @@
 # frozen_string_literal: true
 
 module RecordLoader
-  # :nocov: the version number is loaded before coverage recording starts
-
   # @return [String] The current sem-ver version number
   VERSION = '0.3.0'
-
-  # :nocov:
 end
